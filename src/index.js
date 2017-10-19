c677c8ad7c5b979bab93e43d982c678737eb1023
Stewart Duffy <duffy.stewart@gmail.com>
2017-10-20 09:52:13 +1300

    Static rendering & unregister service worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+import { render } from 'react-snapshot'
 
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+import App from './App'
+import { unregister } from './registerServiceWorker'
+
+render(<App />, document.getElementById('root'))
+unregister()
